e853148cbbf445b02dcfd77dad3142b90014a4ee
LincTech-00 <linc.tech.fiap@gmail.com>
2020-08-21 22:54:55 -0300

    Exercicio desafio pro nosso checkpoint da faculdade.

---
+++ 2_Semestre/Exercicios/Ano_Bisexto.py
@@ -0,0 +1,8 @@
+def informeData(data):
+
+    print("<> *10 ","Digite uma data no formato de DD/MM/AAAA","<> *10 ")
+    data = input("Digite aqui a data: ")
+    return data
+
+# def validarAno():
+#     
